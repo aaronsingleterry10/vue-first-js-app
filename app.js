bae9adf45c5a9d428b45771f8449b20fe10eb9ca
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-09-02 15:58:46 -0500

    finished with this lecture

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const buttonElement = document.querySelector('button');
-const inputElement = document.querySelector('input');
-const listElement = document.querySelector('ul');
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        }
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue)
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app');
 
-function addGoal() {
-    const enteredVaule = inputElement.value;
-    const listItemElement = document.createElement('li');
-    listItemElement.textContent = enteredVaule;
-    listElement.appendChild(listItemElement)
-    inputElement.value = '';
-}
-buttonElement.addEventListener('click', addGoal)
+// const buttonElement = document.querySelector('button');
+// const inputElement = document.querySelector('input');
+// const listElement = document.querySelector('ul');
+
+// function addGoal() {
+//     const enteredVaule = inputElement.value;
+//     const listItemElement = document.createElement('li');
+//     listItemElement.textContent = enteredVaule;
+//     listElement.appendChild(listItemElement)
+//     inputElement.value = '';
+// }
+// buttonElement.addEventListener('click', addGoal)
